dbe77a4f3c9a1a921628bd49cb31bf67ba1c6907
shahwaleed1 <its.waleed33@gmail.com>
2025-01-09 21:46:05 +0500

    added projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,20 +5,23 @@ import { RiLink } from "react-icons/ri";
 const Projects = () => (
   <div className="project-container" id="projects">
     <h1 className="project_heading">Projects</h1>
-    <p className="project_para">Hover on Image to see the Project information. </p>
+    <p className="project_para">
+      Hover on Image to see the Project information.{" "}
+    </p>
     <div className="cards">
       {/* Card 1 */}
       <div className="card card-1">
         <div className="card-i_t">
-          <img src="mod-website.png" alt="Mod APK Website" />
+          <img src="hd_photo.png" alt="HD Photos" />
           <div className="card-text">
-            <h3>Mod APK Website</h3>
+            <h3>HD Photos</h3>
             <p>
-              All apps mod apk file Lorem ipsum dolor sit amet consectetur
-              adipisicing elit.
+              HD Photos is Create in react js and tailwind css.It have a HD
+              Photos to user use in project and wallpapers mobile size, laptop
+              size etc.
             </p>
             <a
-              href="projects/cat website/index.html"
+              href="https://hd-photos.vercel.app/"
               target="_blank"
               rel="noreferrer"
             >
@@ -27,11 +30,36 @@ const Projects = () => (
               </span>
               <span>Visit</span>
             </a>
+            <a href="/" target="_blank" rel="noreferrer">
+              <span>
+                <FaGithub />
+              </span>
+              Github
+            </a>
+          </div>
+        </div>
+      </div>
+      {/* {card 2} */}
+
+      <div className="card card-1">
+        <div className="card-i_t">
+          <img src="coffee.png" alt="coffee" />
+          <div className="card-text">
+            <h3>Shah Coffee</h3>
+            <p>
+              Shah-Coffee website create with react js and tailwind css.it for coffee shop website.
+            </p>
             <a
-              href="projects/cat website/index.html"
+              href="https://shahcoffee.vercel.app/"
               target="_blank"
               rel="noreferrer"
             >
+              <span>
+                <RiLink />
+              </span>
+              <span>Visit</span>
+            </a>
+            <a href="/" target="_blank" rel="noreferrer">
               <span>
                 <FaGithub />
               </span>
