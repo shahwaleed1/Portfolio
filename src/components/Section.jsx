9334915e009808b5ee7e79ec33fc3a56b6cea003
shahwaleed1 <its.waleed33@gmail.com>
2025-01-12 00:32:17 +0500

    navbar added

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,7 +1,32 @@
 import React, { useEffect, useState } from "react";
-
+import { AiFillHome } from "react-icons/ai";
+import { IoMdInformationCircle } from "react-icons/io";
+import { FaGear } from "react-icons/fa6";
+import { LuBrainCircuit } from "react-icons/lu";
+import { FaAddressBook } from "react-icons/fa";
 
 const Section = () => {
+
+  const [isScreenWidthSmall, setIsScreenWidthSmall] = useState(false);
+
+  useEffect(() => {
+    const checkScreenWidth = () => {
+      setIsScreenWidthSmall(window.innerWidth <= 800);
+    };
+
+    // Check screen width on component mount
+    checkScreenWidth();
+
+    // Add event listener for screen resizing
+    window.addEventListener("resize", checkScreenWidth);
+
+    // Cleanup the event listener on component unmount
+    return () => {
+      window.removeEventListener("resize", checkScreenWidth);
+    };
+  }, []);
+
+
 
   // const words = ['WEB DEVELOPER', 'WEB DESIGNER', 'UI/UX DESIGNER'];
   // const [currentWordIndex, setCurrentWordIndex] = useState(0);
@@ -29,48 +54,68 @@ const Section = () => {
   //   return () => clearInterval(typingInterval);
   // }, [displayedText, isDeleting, currentWordIndex]);
 
-
-  return(
-  <section>
-    <div className="sec-container">
-      <video className="bg-video" autoPlay muted loop src="5200-183786525_medium.mp4"></video>
-      <div className="container">
-        <div className="img-bg-div">
-          <img src="profile_pic.png" alt="Admin logo" className="admin-logo" />
-          <div className="img-back"></div>
-        </div>
-        <div className="heading">
-          <h1 className="name">Waleed Shah</h1>
-          <p><strong>Frontend Developer</strong></p>
-          {/* <p>{displayedText}</p> */}
-          <small>
-            My name is Waleed Shah, I Frontend Developer from Pakistan <br /> Freelancer, Company Job <br />Web
-            Languages
-          </small>
-        </div>
-        <div className="marquee-container">
-          <div className="marquee">
-            <img src="Image/pngegg (9).png" alt="" />
-            <img src="Image/pngegg (12).png" alt="" />
-            <img src="Image/pngegg (11).png" alt="" id="bs" />
+  return (
+    <section>
+      <div className="sec-container">
+        <video
+          className="bg-video"
+          autoPlay
+          muted
+          loop
+          src="5200-183786525_medium.mp4"
+        ></video>
+        <div className="container">
+          <div className="img-bg-div">
+            <img
+              src="profile_pic.png"
+              alt="Admin logo"
+              className="admin-logo"
+            />
+            <div className="img-back"></div>
+          </div>
+          <div className="heading">
+            <h1 className="name">Waleed Shah</h1>
+            <p>
+              <strong>Frontend Developer</strong>
+            </p>
+            <small>
+              My name is Waleed Shah, I Frontend Developer from Pakistan <br />
+              Freelancer, Company Job <br />
+              Web Languages
+            </small>
+          </div>
+          <div className="marquee-container">
+            <div className="marquee">
+              <img src="Image/pngegg (9).png" alt="" />
+              <img src="Image/pngegg (12).png" alt="" />
+              <img src="Image/pngegg (11).png" alt="" id="bs" />
+            </div>
           </div>
         </div>
+        {isScreenWidthSmall && (
+          <div className="link-div">
+            <div className="icons">
+              <a href="">
+                <AiFillHome />
+              </a>
+              <a href="">
+                <IoMdInformationCircle />
+              </a>
+              <a href="">
+                <FaGear />
+              </a>
+              <a href="">
+                <LuBrainCircuit />
+              </a>
+              <a href="">
+                <FaAddressBook />
+              </a>
+            </div>
+          </div>
+        )}
       </div>
-      <div className="link-div">
-        <div className="icons">
-          <a href="#"><i className="fa-solid fa-house"></i></a>
-          <a href="#about"><i className="fa-solid fa-circle-info"></i></a>
-          <a href="#projects"><i className="fa-solid fa-gears"></i></a>
-          <a href="#skill"><i className="fa-solid fa-brain"></i></a>
-          <a href="#contact"><i className="fa-solid fa-address-book"></i></a>
-        </div>
-        <div className="icon-text">
-          <span>Home</span> <span>About</span> <span>Projects</span> <span>Skills</span> <span>Contact</span>
-        </div>
-      </div>
-    </div>
-  </section>
-  )
+    </section>
+  );
 };
 
 export default Section;
