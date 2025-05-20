89a92d2bbf191c881c1f34cc5655756392325c1d
shahwaleed1 <its.waleed33@gmail.com>
2025-05-20 21:44:49 +0500

    footer set

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,9 @@
 import React from "react";
-import "./Footer.css"; // Import the CSS file for styling
+import "./Footer.css";
 import { FaLinkedin, FaGithub, FaTwitter, FaEnvelope } from "react-icons/fa";
+import { IoLogoWhatsapp } from "react-icons/io";
+
+
 
 const Footer = () => {
   return (
@@ -11,23 +14,26 @@ const Footer = () => {
         </div>
         <div className="footer-center">
           <div className="social-icons">
-            <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer" className="social-icon">
+            <a href="https://www.linkedin.com/in/waleed-shah-4a1a52357/" target="_blank" rel="noopener noreferrer" className="social-icon">
               <FaLinkedin />
             </a>
-            <a href="https://github.com" target="_blank" rel="noopener noreferrer" className="social-icon">
+            <a href="https://github.com/shahwaleed1" target="_blank" rel="noopener noreferrer" className="social-icon">
               <FaGithub />
             </a>
-            <a href="https://twitter.com" target="_blank" rel="noopener noreferrer" className="social-icon">
+            {/* <a href="https://twitter.com" target="_blank" rel="noopener noreferrer" className="social-icon">
               <FaTwitter />
-            </a>
+            </a> */}
             <a href="mailto:waleeddev4@gmail.com" className="social-icon">
               <FaEnvelope />
             </a>
+            {/* <a href="" target="_blank" rel="noopener noreferrer" className="social-icon">
+              <IoLogoWhatsapp />
+            </a> */}
           </div>
         </div>
-        <div className="footer-right">
+        {/* <div className="footer-right">
           <p><a href="#contact">Contact Me</a></p>
-        </div>
+        </div> */}
       </div>
     </footer>
   );
