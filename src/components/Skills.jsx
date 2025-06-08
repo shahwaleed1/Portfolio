68e1fc4cd5cdb1b0848f4ac1790680ab067a0729
shahwaleed1 <its.waleed33@gmail.com>
2025-06-08 12:17:26 +0500

    progress add

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,5 +1,53 @@
 import React from "react";
 
+const techskill = [
+  {
+    id: 1,
+    name: 'HTML',
+    value: 80
+  },
+  {
+    id: 2,
+    name: 'CSS',
+    value: 90
+  },
+  {
+    id: 3,
+    name: 'Bootstrap',
+    value: 80
+  },
+  {
+    id: 4,
+    name: 'javascript',
+    value: 70
+  },
+  {
+    id: 5,
+    name: 'React',
+    value: 80
+  },
+  {
+    id: 6,
+    name: 'Tailwindcss',
+    value: 80
+  },
+  {
+    id: 7,
+    name: 'Node.js',
+    value: 70
+  },
+  {
+    id: 8,
+    name: 'MongoDB',
+    value: 60
+  },
+  {
+    id: 9,
+    name: 'Express.js',
+    value: 60
+  }
+]
+
 const Skills = () => (
   <div className="skill-container" id="skill">
     <h1>Skills</h1>
@@ -35,7 +83,23 @@ const Skills = () => (
         </div>
       </div>
     </div>
-  </div>
+
+
+    <div className="progres_main_div">
+      {techskill.map((data) => (
+        <div className="progress-container" key={data.id}>
+          <span className="tech-name">{data.name}</span>
+          <div className="progress-bar-wrapper">
+            <div
+              className="progress"
+              style={{ width: `${data.value}%` }}
+              data-value={`${data.value}%`}
+            ></div>
+          </div>
+        </div>
+      ))}
+    </div>
+  </div >
 );
 
 export default Skills;
