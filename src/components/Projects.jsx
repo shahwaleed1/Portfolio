1f3aa544430af44c4f5350a1c89e51f99b07e818
shahwaleed1 <its.waleed33@gmail.com>
2025-09-08 13:19:27 +0500

    projects data into json formate

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,59 @@
 import React from "react";
 import Card from "./Card";
 
+
+const projects = [
+  {
+    name: "HD Photos",
+    description: "HD Photos is Create in react js and tailwind css.It have a HDPhotos to user use in project and wallpapers mobile size, laptopsize etc.",
+    live: "https://hd-photos.vercel.app/",
+    image: "./hd_photo.png"
+  },
+  {
+    name: "Shah Coffee",
+    description: "Shah Coffee",
+    live: "https://shahcoffee.vercel.app/",
+    image: "./coffee.png"
+  },
+  {
+    name: "Virtual Assistant",
+    description: "This Shahni virtual assistant dynamically give response of any Question.",
+    live: "https://waleed-assistan.vercel.app/",
+    image: "./Ai.png"
+  },
+  {
+    name: "Weather App",
+    description: "Weather app with searchbox.",
+    live: "https://shahwaleed1.github.io/WeatherApp_js/",
+    image: "./weather_js.png"
+  },
+  {
+    name: "Food Nutrient",
+    description: "The Food-Nutrient WebApp is a modern and responsive web application.",
+    live: "https://food-nutrient.vercel.app/",
+    image: "./Food_Nutrient.png"
+  },
+  {
+    name: "GTA 6",
+    description: "GTA 6 website using React js and Tailwind css.",
+    live: "https://gta-6-delta.vercel.app/",
+    image: "./gta6.png"
+  },
+  {
+    name: "Blog",
+    description: "Beauty of Pakistan is a stylish and intuitive blogger website designed to immerse visitors in the rich landscapes and culture of Pakistan. It includes essential pages like Home and About, along with Login and Signup functionality for user interaction.",
+    live: "https://blogapp-z729.vercel.app/",
+    image: "./blog_project-min.png"
+  }
+];
+
+const tech = [
+  {
+    
+  }
+]
+
+
 const Projects = () => (
   <div className="project-container" id="projects">
     <h1 className="project_heading">Projects</h1>
